5e5e5a5ec400f5643bd87097f4567f823b845e3b
Jayvee N Mapote <101295973+JvMapote@users.noreply.github.com>
2022-06-06 23:29:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+def selection_sort(nums):
+    for i in range(6):
+        minpos = i
+        for j in range(i,7):
+            if nums[j] < nums[minpos]:
+                minpos = j
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+        print(nums)
+nums = [2, 8, 5, 3, 9, 4, 1]
+selection_sort(nums)
+print("\nSelection Sort: ")
+print(nums)
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        insertion = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > insertion:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = insertion
+arr = [2, 8, 5, 3, 9, 4, 1]
+insertion_sort(arr)
+print("Insertion Sort: ")
+print(arr)
+
